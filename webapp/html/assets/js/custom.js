f429769b4831f4a98107a131fd564effc99af8f8
harshbodhi03 <harsh.bodhi@gmail.com>
2024-03-01 16:32:20 +0400

    Initial web app commit

---
+++ webapp/html/assets/js/custom.js
@@ -0,0 +1,148 @@
+// ============= side bar start ============= // 
+function openNav() {
+    "use strict";
+    const sidepanel = document.getElementById("mySidepanel");
+    if (sidepanel) {
+        sidepanel.style.left = "0";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+
+function closeNav() {
+    "use strict";
+    const sidepanel = document.getElementById("mySidepanel");
+    if (sidepanel) {
+        sidepanel.style.left = "-320px";
+    } else {
+        console.error("Error: Side panel element not found!");
+    }
+}
+// ============= side bar end ============= //
+
+// button back to top start
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("backToTopBtn").style.display = "block";
+    } else {
+        document.getElementById("backToTopBtn").style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    const scrollToTopBtn = document.documentElement || document.body;
+    scrollToTopBtn.scrollIntoView({
+        behavior: "smooth"
+    });
+}
+// button back to top end
+
+// footer validation start
+const fom = document.getElementById('footer-form');
+const footerMessage = document.getElementById('footer-message');
+
+fom.addEventListener('submit', (event) => {
+    event.preventDefault();
+    footerMessage.innerHTML = 'Form submitted success fully!';
+    footerMessage.style.display = 'block';
+    fom.reset();
+    setTimeout(() => {
+        footerMessage.style.display = 'none';
+    }, 3000);
+});
+// footer validation end
+
+// ============= carousel start
+$(document).ready(function() {
+    var slider = $('.slider');
+
+    slider.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        dots: true,
+    });
+
+    // Pause the slider on hover
+    slider.on('mouseenter', function() {
+        slider.slick('slickPause');
+    });
+
+    // Resume the slider when not hovering
+    slider.on('mouseleave', function() {
+        slider.slick('slickPlay');
+    });
+});
+
+// ============= carousel end
+
+// ============= counter start ============= // 
+
+(function(e) {
+    "use strict";
+    e.fn.counterUp = function(t) {
+        var n = e.extend({
+            time: 400,
+            delay: 10
+        }, t);
+        return this.each(function() {
+            var t = e(this),
+                r = n,
+                i = function() {
+                    var e = [],
+                        n = r.time / r.delay,
+                        i = t.text(),
+                        s = /[0-9]+,[0-9]+/.test(i);
+                    i = i.replace(/,/g, "");
+                    var o = /^[0-9]+$/.test(i),
+                        u = /^[0-9]+\.[0-9]+$/.test(i),
+                        a = u ? (i.split(".")[1] || [])
+                        .length : 0;
+                    for (var f = n; f >= 1; f--) {
+                        var l = parseInt(i / n * f);
+                        u && (l = parseFloat(i / n * f)
+                            .toFixed(a));
+                        if (s)
+                            while (/(\d+)(\d{3})/.test(l.toString())) l = l.toString()
+                                .replace(/(\d+)(\d{3})/, "$1,$2");
+                        e.unshift(l)
+                    }
+                    t.data("counterup-nums", e);
+                    t.text("0");
+                    var c = function() {
+                        t.text(t.data("counterup-nums")
+                            .shift());
+                        if (t.data("counterup-nums")
+                            .length) setTimeout(t.data("counterup-func"), r.delay);
+                        else {
+                            delete t.data("counterup-nums");
+                            t.data("counterup-nums", null);
+                            t.data("counterup-func", null)
+                        }
+                    };
+                    t.data("counterup-func", c);
+                    setTimeout(t.data("counterup-func"), r.delay)
+                };
+            t.waypoint(i, {
+                offset: "100%",
+                triggerOnce: !0
+            })
+        })
+    }
+})(jQuery);
+
+jQuery(document)
+    .ready(function($) {
+        $('.count')
+            .counterUp({
+                delay: 10,
+                time: 2000
+            });
+    });
+// ============= counter end ============= //
